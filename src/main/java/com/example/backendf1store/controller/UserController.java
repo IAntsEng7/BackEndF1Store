e184e69744af217500b7d680781fce71de1c20ba
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-25 01:09:42 +0800

    Register

--- src/main/java/com/example/backendf1store/controller/UserController.java
+++ src/main/java/com/example/backendf1store/controller/UserController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class UserController {
   @GetMapping("/allUser")
   public List<User> getAllUser() {
     return userRepo.findAll();
+  }
+
+  // Register
+  @PostMapping("/register")
+  public User createUser(@RequestBody User user){
+    return userRepo.save(user);
   }
 }
