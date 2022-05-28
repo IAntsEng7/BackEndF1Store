3b2a4975b645d0b2dd3b1cdbb13c39b487877a3d
IAntsEng7 <83633994+IAntsEng7@users.noreply.github.com>
2022-05-28 17:09:52 +0800

    Driver model (getter / setter)

--- src/main/java/com/example/backendf1store/model/Driver.java
+++ src/main/java/com/example/backendf1store/model/Driver.java
@@ -35,4 +35,68 @@ public class Driver {
 
   @Column(name="describe")
   private String describe;
+
+  public long getDId() {
+    return dId;
+  }
+
+  public void setDId(long dId) {
+    this.dId = dId;
+  }
+
+  public String getDriverNumber() {
+    return driverNumber;
+  }
+
+  public void setDriverNumber(String driverNumber) {
+    this.driverNumber = driverNumber;
+  }
+
+  public String getTeam() {
+    return team;
+  }
+
+  public void setTeam(String team) {
+    this.team = team;
+  }
+
+  public String getPodiums() {
+    return podiums;
+  }
+
+  public void setPodiums(String podiums) {
+    this.podiums = podiums;
+  }
+
+  public String getDriverName() {
+    return driverName;
+  }
+
+  public void setDriverName(String driverName) {
+    this.driverName = driverName;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public String getPicUrl() {
+    return picUrl;
+  }
+
+  public void setPicUrl(String picUrl) {
+    this.picUrl = picUrl;
+  }
+
+  public String getDescribe() {
+    return describe;
+  }
+
+  public void setDescribe(String describe) {
+    this.describe = describe;
+  }
 }
